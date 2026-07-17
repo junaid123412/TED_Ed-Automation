495c6ab7a57201cb06e2184ff51f4c699c893087
junaid123412 <Junaidhussainsqa@gmail.com>
2026-07-17 11:20:51 +0500

    Upload playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,32 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { envConfig } from './src/utils/envConfig';
+
+const config: PlaywrightTestConfig = {
+  timeout: 30000,
+  expect: {
+    timeout: 5000,
+  },
+  retries: envConfig.isCI ? 1 : 0,
+  use: {
+    baseURL: envConfig.baseUrl,
+    headless: envConfig.headless,
+    screenshot: 'only-on-failure',
+    video: 'on-first-retry',
+    trace: 'on-first-retry',
+    actionTimeout: 10000,
+    ignoreHTTPSErrors: true,
+    httpCredentials: {
+      username: envConfig.basicAuthUsername,
+      password: envConfig.basicAuthPassword,
+    },
+  },
+  reporter: [['html', { outputFolder: 'reports/playwright-report', open: 'never' }]],
+  projects: [
+    {
+      name: 'chromium',
+      use: { browserName: 'chromium' },
+    },
+  ],
+};
+
+export default config;
