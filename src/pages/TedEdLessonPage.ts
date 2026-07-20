ccd905765474b47abe0b57766596348603d39aaa
Junaid Hussain <Junaidhussainsqa@gmail.com>
2026-07-20 15:12:48 +0500

    Upload src/pages/TedEdLessonPage.ts

--- src/pages/TedEdLessonPage.ts
+++ src/pages/TedEdLessonPage.ts
@@ -209,10 +209,10 @@ export class TedEdLessonPage {
     if (loggedOut) {
       await this.clickSignIn();
       await this.clickEmailInputField();
-      await this.enterEmail('junaid.hussain+1@kualitatem.com');
+      await this.enterEmail(envConfig.tedLoginEmail);
       await this.clickEmailContinue();
       await this.clickPasswordInputField();
-      await this.enterPassword('test@112233');
+      await this.enterPassword(envConfig.tedLoginPassword);
       await this.clickPasswordContinue();
       await this.acceptConsentIfVisible();
     }
