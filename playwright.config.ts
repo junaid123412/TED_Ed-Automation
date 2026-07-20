bda3619d8d088f71ff23cbb4f68a8fe7c19f8468
Junaid Hussain <Junaidhussainsqa@gmail.com>
2026-07-20 15:32:57 +0500

    Upload playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,6 +26,14 @@ const config: PlaywrightTestConfig = {
       name: 'chromium',
       use: { browserName: 'chromium' },
     },
+    {
+      name: 'firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'webkit',
+      use: { browserName: 'webkit' },
+    },
   ],
 };
 
