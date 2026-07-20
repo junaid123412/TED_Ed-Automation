4803e45cfee5eba89f445e41539472a5332d1067
Junaid Hussain <Junaidhussainsqa@gmail.com>
2026-07-20 15:13:12 +0500

    Upload src/utils/envConfig.ts

--- src/utils/envConfig.ts
+++ src/utils/envConfig.ts
@@ -6,10 +6,10 @@ const baseUrl = process.env.BASE_URL || 'https://teded-integration.herokuapp.com
 const headless = process.env.HEADLESS?.toLowerCase() !== 'false';
 const isCI = process.env.CI === 'true' || process.env.CI === '1';
 const retries = isCI ? 1 : 0;
-const basicAuthUsername = process.env.HTTP_AUTH_USERNAME || 'teduser';
-const basicAuthPassword = process.env.HTTP_AUTH_PASSWORD || 'tUeygHu@4q';
-const tedLoginEmail = process.env.TED_LOGIN_EMAIL || 'junaid.hussain+1@kualitatem.com';
-const tedLoginPassword = process.env.TED_LOGIN_PASSWORD || 'test@112233';
+const basicAuthUsername = process.env.HTTP_AUTH_USERNAME || '';
+const basicAuthPassword = process.env.HTTP_AUTH_PASSWORD || '';
+const tedLoginEmail = process.env.TED_LOGIN_EMAIL || '';
+const tedLoginPassword = process.env.TED_LOGIN_PASSWORD || '';
 
 export const envConfig = {
   baseUrl,
